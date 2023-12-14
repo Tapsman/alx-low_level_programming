b2a0ae64e1d74d8ce4e24439eda92eaa74c819eb
Tapsman <tapiwanyoni140@gmail.com>
2023-12-14 11:43:55 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when success
+ */
+int mai(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
