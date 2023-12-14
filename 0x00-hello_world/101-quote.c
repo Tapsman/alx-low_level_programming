cb7cb1f514ec6411a2465fa4a08b91440d7b4128
Tapsman <tapiwanyoni140@gmail.com>
2023-12-14 13:04:49 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - a program that prints a line to standard error
  * Return: 1 (success)
  */
-int main(voi)
+int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
