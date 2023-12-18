ed9dd5d6a329a36bff794e9ea3b4a7aa8feeb9ce
Tapsman <tapiwanyoni140@gmail.com>
2023-12-18 10:47:04 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.>
+/**
+ * main - program to print all numbers of base 16 in lowercase
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0' ; d <= '9' ; d++)
+	putchar(d);
+
+	for (low = 'a' ; low <= 'f' ; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
