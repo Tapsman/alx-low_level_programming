f930d279922f3470a32f53d6ffe754e125a5a4d9
Tapsman <tapiwanyoni140@gmail.com>
2023-12-21 12:11:36 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "mai.h"
+/**
+ * _isdigit.c - Checks for digit from 0 to 9
+ * @c: Function parameter
+ * Return: 1 on success 0 on failure
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && C <= 57)
+		return (1);
+	else
+		return (0);
+}
