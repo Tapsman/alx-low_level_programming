dc504cf7f4fc5d0220945b411a79972a68c695f2
Tapsman <tapiwanyoni140@gmail.com>
2024-01-02 14:36:46 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_array - A function that prints n elements of an array
- * a: array name
+ * @a: array name
  * @n: I s the number of elements of the array to be printed
  * Return: a and n input
  */
