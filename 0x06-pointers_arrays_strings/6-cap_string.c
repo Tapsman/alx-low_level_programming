6575d126fb435430cb43e78bcce861175378675e
Tapsman <tapiwanyoni140@gmail.com>
2024-01-05 11:59:23 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,6 +20,8 @@ char *cap_string(char *str)
 	str[index - 1] == ';' ||
 	str[index - 1] == '.' ||
 	str[index - 1] == '!' ||
+	str[index - 1] == '?' ||
+	str[index - 1] == '"' ||
 	str[index - 1] == '(' ||
 	str[index - 1] == ')' ||
 	str[index - 1] == '{' ||
