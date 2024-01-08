6cceaa1436ccdfd60b1d8590d43996d85daf0266
Tapsman <tapiwanyoni140@gmail.com>
2024-01-08 12:34:49 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,7 +12,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
-		for (n = 0 ; accept[i] != '\0'; n++)
+		for (n = 0 ; accept[n] != '\0'; n++)
 		{
 			if (s[i] == accept[n])
 				return (s + i);
