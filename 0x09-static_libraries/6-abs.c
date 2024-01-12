cdb19dd78efac8117f9d6a88a9fa8544ae6e088b
Tapsman <tapiwanyoni140@gmail.com>
2024-01-12 11:06:33 -0800

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @c: Number to be computed
+ * Return: Absolute vale of a number or zero
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
