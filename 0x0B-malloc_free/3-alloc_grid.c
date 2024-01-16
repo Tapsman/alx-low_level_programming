c97033c4068a749d30450d611b98ae64dd744d3b
Tapsman <tapiwanyoni140@gmail.com>
2024-01-16 12:59:24 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (len <= 0)
 		return (NULL);
 
-	array = ((int  **)malloc(sizeof(int *) * height));
+	array = (int  **)malloc(sizeof(int *) * height);
 	if (array == NULL)
 		return (NULL);
 
@@ -26,7 +26,8 @@ int **alloc_grid(int width, int height)
 		if (array[i] == NULL)
 		{
 			for (i-- ; i >= 0 ; i--)
-				free(array);
+				free(array[i]);
+			free(array);
 			return (NULL);
 		}
 	}
