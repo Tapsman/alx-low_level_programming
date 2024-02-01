1a3724bdb5af54ab751d02f81cc63d69c5b0d549
Tapsman <tapiwanyoni140@gmail.com>
2024-02-01 13:16:03 -0800

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * free_list A function that frees list_t list
+ * free_list - A function that frees list_t list
  * @head: Pointer to the first node
  */
 void free_list(list_t *head)
