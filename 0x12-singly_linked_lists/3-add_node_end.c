3c8fd0f83c7285c9d3b1e1a4fd5afc6a6c19d184
Tapsman <tapiwanyoni140@gmail.com>
2024-02-01 13:00:54 -0800

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,6 +10,45 @@ unsigned int _strlen(char *str)
 
 	for (i = 0 ; str[i] ; i++)
 		;
-	return(i);
+	return (i);
 }
+/**
+ * add_node_end - Adds a new node at the end of
+ * a list_t list
+ * @head: Double pointer to linked list
+ * @str: The string to add on new node
+ * Return: Pointer to new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
 
+	if (str == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+
+	return (new);
+}
