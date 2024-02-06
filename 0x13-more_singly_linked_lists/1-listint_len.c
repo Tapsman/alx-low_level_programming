0c645eeca77ebe3f9420beea171065387d82921f
Tapsman <tapiwanyoni140@gmail.com>
2024-02-06 07:55:02 -0800

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len - A function that returns the number
+ * of elements in a linked listint_t list
+ * @h: The linked lists to traverse
+ * Return: The number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
