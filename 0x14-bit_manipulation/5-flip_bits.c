47a561abd7ea4883a71b4eb6d8ef27c1aee8d7d9
Tapsman <tapiwanyoni140@gmail.com>
2024-02-08 12:22:52 -0800

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
 
-	for (c = 63 ; c >= 0 ; c++)
+	for (c = 63 ; c >= 0 ; c--)
 	{
 		current = exclusive >> c;
 		if (current & 1)
