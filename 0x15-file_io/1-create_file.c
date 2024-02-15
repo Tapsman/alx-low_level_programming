4e59c5dbddc1dc4aab73aeec18edcececf0e4bb5
Tapsman <tapiwanyoni140@gmail.com>
2024-02-15 09:59:06 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,10 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for (nletters = 0 ; text_content[nletters] ; nletters++)
-
+		;
 	rwr = write(file_b, text_content, nletters);
 	if (rwr == -1)
 		close(file_b);
 
-	return (-1);
+	return (1);
 }
