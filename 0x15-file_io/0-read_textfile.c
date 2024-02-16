31faa9f4de30622da3f36be83fa78f9cefc5a5ba
Tapsman <tapiwanyoni140@gmail.com>
2024-02-16 12:56:03 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,33 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
 /**
  * read_textfile - A function that reads the text file and prints it
  * to the POSIX stdo
  * @filename: The name of the file to read
  * @letters: The number of letters to read
- * Return: The number of letters it can read and print
+ * Return: Actual number of letters to print and read
+ *  or 0 if the function fails or filename is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_b;
-	ssize_t lenr, lenw;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 	char *buffer;
 
-	if (filename == NULL)
-		return (0);
-
-	file_b = open(filename, O_RDONLY);
-	if (file_b == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(file_b);
-		return (0);
-	}
+	t = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, t);
 
-	lenr = read(file_b, buffer, letters);
-	close(file_b);
-	if (lenr == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-	if (lenr != lenw)
-		return (0);
-	return (lenw);
+	close(fd);
+	return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,8 +12,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void check_IO_stat(int stat, int fd, char *filename, char node);
-int main(int argc, char *argv[]);
 void check_IO_stat(int stat, int fd, char *filename, char mode);
 int _strlen(char *str);
 #endif
