e4385669706e1571f556a0cefa3606c5e6683590
Tapsman <tapiwanyoni140@gmail.com>
2024-03-26 10:03:16 -0700

    opperators.c

---
+++ 0x18-dynamic_libraries/opperators.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+/**
+ * add - A function that will add two integers
+ * @a: Is the first integer to be added
+ * @b: Is the second integer to be added
+ * Return: Always return the answer in the function
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - A function that will subtract two integers
+ * @a: Is the first integer to be added
+ * @b: Is the second integer to be added
+ * Return: Always the answer in the function
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - A function that will multiply two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: Always the answer in the function
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - A function that divides the two integers
+ * @a: The first integer
+ * @b: Is the second integer
+ * Return: Always the answer in the function
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - A function that divides two integers and
+ * gives the remainder
+ * @a: The first integer
+ * @b: The second integer
+ * Return: Always the answer in the function
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
