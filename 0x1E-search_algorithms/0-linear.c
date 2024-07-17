0f77d7195b4c72c07c3ae5883fda841b9d6aca8b
Tapsman <tapiwanyoni140@gmail.com>
2024-07-17 13:11:25 -0700

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,21 +4,21 @@
  * an array of integers using linear serach
  * @array: Is the pointer to the first element of the array to search
  * @size: Is the size of the elements in the array
- * @value: IS the value that will be searched
+ * @value: Is the value that will be searched
  * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int t;
+int lsg;
 
-	if (array == NULL)
-		return (-1);
+if (array == NULL)
+return (-1);
 
-	for (t = 0 ; t < (int)size ; t++)
-	{
-		printf("Value checked array[%u] = [%d]\n", t, array[t]);
-	if (value == array[t])
-		return (t);
-	}
-	return (-1);
+for (lsg = 0 ; lsg < (int)size ; lsg++)
+{
+	printf("Value checked array[%u] = [%d]\n", lsg, array[lsg]);
+	if (value == array[lsg])
+		return (lsg);
+}
+return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -38,4 +38,5 @@ int recursive_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int recursive_helper(int *array, size_t size, int value, size_t ix);
 #endif
